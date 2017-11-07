aa773beba93631e8ecf0666aaebc8f9b0ad0bc32
Adam Furbee <adam.furbee@gmail.com>
2017-11-07 11:03:53 -0600

    refactory sync.py to upload scripts truely asynchronously

--- sync.py
+++ sync.py
@@ -28,28 +28,35 @@ async def upload_scripts(session, url, user, passwd):
     mypath = dirname(realpath(__file__))
     scripts = [f.name for f in os.scandir(join(mypath, 'scripts'))
                if f.is_file() and f.name.split('.')[-1] in supported_script_extensions]
+    tasks = []
+    for script in scripts:
+        task = asyncio.ensure_future(upload_script(session, url, user, passwd, script))
+        tasks.append(task)
+    responses = await asyncio.gather(*tasks)
 
+
+async def upload_script(session, url, user, passwd, script):
+    mypath = dirname(realpath(__file__))
     auth = aiohttp.BasicAuth(user, passwd)
     headers = {'content-type': 'application/xml'}
-
-    for script in scripts:
-        with open(join(mypath, 'scripts/' + script), 'r') as f:
-            data=f.read()
-        with async_timeout.timeout(10):
-            async with session.get(url + '/JSSResource/scripts/name/' + script,
-                    auth=auth) as resp:
-                template = await get_script_template(session, url, user, passwd, script)
-                template.find('script_contents').text = data
-                if resp.status == 200:
-                    put_url = url + '/JSSResource/scripts/name/' + script
-                    async with session.put(put_url, auth=auth, data=ET.tostring(template), headers=headers) as response:
-                        print(response.status)
-                        return response.status
-                else:
-                    post_url = url + '/JSSResource/scripts/id/0'
-                    async with session.post(post_url, auth=auth, data=ET.tostring(template), headers=headers) as response:
-                        print(response.status)
-                        return response.status
+    with open(join(mypath, 'scripts/' + script), 'r') as f:
+        data=f.read()
+    with async_timeout.timeout(10):
+        async with session.get(url + '/JSSResource/scripts/name/' + script,
+                auth=auth) as resp:
+            template = await get_script_template(session, url, user, passwd, script)
+            template.find('script_contents').text = data
+            print(ET.tostring(template))
+            if resp.status == 200:
+                put_url = url + '/JSSResource/scripts/name/' + script
+                async with session.put(put_url, auth=auth, data=ET.tostring(template), headers=headers) as response:
+                    print(response.status)
+                    return response.status
+            else:
+                post_url = url + '/JSSResource/scripts/id/0'
+                async with session.post(post_url, auth=auth, data=ET.tostring(template), headers=headers) as response:
+                    print(response.status)
+                    return response.status
 
 
 async def get_script_template(session, url, user, passwd, script):
@@ -67,9 +74,9 @@ async def get_script_template(session, url, user, passwd, script):
                         template = ET.fromstring(await response.text())
                 else:
                     template = ET.parse(join(mypath, 'templates/script.xml')).getroot()
-    if template.find('name') is None:
+    if not template.find('name'):
         ET.SubElement(template, 'name').text = script
-    if template.find('filename') is None:
+    if not template.find('filename'):
         ET.SubElement(template, 'filename').text = script
     # print(ET.tostring(template))
     return template
