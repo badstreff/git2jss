255832d296e6abee905e886bea1c57712b776911
Rusty Myers <rustymyers@gmail.com>
2023-10-30 10:34:10 -0400

    Fixes for paths, passwords, and padding (#40)
    * Adding config file
    
    * Adding Requirements, configparser to download.py
    
    * Adding token auth, configparser, and export_path
    
    * Adding token auth, configparser, and sync_path
    
    * Update sync.py
    
    Adding sync_path in missing spots
    
    * Update sync.py
    
    Removing empty config file creation so script won't fail subsequent runs.
    
    * Update sync.py
    
    Comments fixed for configparser
    
    * Update README.md
    
    Adding configparser notes
    
    * Update download.py
    
    Fixing configparser creating empty file
    
    * Update sync.py
    
    Updating configparser section to try each value, but fail gracefully. Moved default sync_path to prior so it will be set before parser and configparser values are read.
    
    * Update download.py
    
    Updating configparser and export_path to allow default to be set before parser and configparser
    
    * Black formatter
    
    * Updated download.py using Black auto formatter
    
    * Removnig missing config file message
    
    * Fixing "is not" testing a litereal to "!="

--- tools/download.py
+++ tools/download.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import getpass
 import requests
 from xml.etree import ElementTree as ET
@@ -11,27 +11,32 @@ import configparser
 # Suppress the warning in dev
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 # https://github.com/lazymutt/Jamf-Pro-API-Sampler/blob/5f8efa92911271248f527e70bd682db79bc600f2/jamf_duplicate_detection.py#L99
 def get_uapi_token():
-    '''
+    """
     fetches api token
-    '''
+    """
     jamf_test_url = url + "/api/v1/auth/token"
     response = requests.post(url=jamf_test_url, auth=(username, password))
     response_json = response.json()
-    return response_json['token']
+    return response_json["token"]
 
 
 def invalidate_uapi_token(uapi_token):
-    '''
+    """
     invalidates api token
-    '''
+    """
     jamf_test_url = url + "/api/v1/auth/invalidate-token"
-    headers = {'Accept': '*/*', 'Authorization': 'Bearer ' + uapi_token}
+    headers = {"Accept": "*/*", "Authorization": "Bearer " + uapi_token}
     _ = requests.post(url=jamf_test_url, headers=headers)
 
-def download_scripts(mode, overwrite=None,):
-    """ Downloads Scripts to ./scripts and Extension Attributes to ./extension_attributes
+
+def download_scripts(
+    mode,
+    overwrite=None,
+):
+    """Downloads Scripts to ./scripts and Extension Attributes to ./extension_attributes
 
     Folder Structure:
     ./scripts/script_name/script.sh
@@ -54,121 +59,141 @@ def download_scripts(mode, overwrite=None,):
     """
 
     # Set various values based on resource type
-    if mode == 'ea':
-        resource = 'computerextensionattributes'
-        download_path = 'extension_attributes'
-        script_xml = 'input_type/script'
+    if mode == "ea":
+        resource = "computerextensionattributes"
+        download_path = "extension_attributes"
+        script_xml = "input_type/script"
 
-    if mode == 'script':
-        resource = 'scripts'
-        download_path = 'scripts'
-        script_xml = 'script_contents'
+    if mode == "script":
+        resource = "scripts"
+        download_path = "scripts"
+        script_xml = "script_contents"
 
     token = get_uapi_token()
     # Get all IDs of resource type
-    r = requests.get(url + '/JSSResource/%s' %resource,
-        headers= {'Accept': 'application/xml',
-                  'Content-Type': 'application/xml',
-                  'Authorization': 'Bearer ' + token},
-            verify=args.do_not_verify_ssl)
+    r = requests.get(
+        url + "/JSSResource/%s" % resource,
+        headers={
+            "Accept": "application/xml",
+            "Content-Type": "application/xml",
+            "Authorization": "Bearer " + token,
+        },
+        verify=args.do_not_verify_ssl,
+    )
 
     # Basic error handling
     if r.status_code != 200:
-        print("Something went wrong with the request, check your password and privileges and try again. \n \
+        print(
+            "Something went wrong with the request, check your password and privileges and try again. \n \
         It's also possible that the url is incorrect. \n \
-        Here is the HTTP Status code: %s" % r.status_code)
+        Here is the HTTP Status code: %s"
+            % r.status_code
+        )
         exit(1)
     tree = ET.fromstring(r.content)
-    resource_ids = [ e.text for e in tree.findall('.//id') ]
+    resource_ids = [e.text for e in tree.findall(".//id")]
 
     # Download each resource and save to disk
     for resource_id in resource_ids:
         get_script = True
 
-        r = requests.get(url + '/JSSResource/%s/id/%s' % (resource,resource_id),
-            headers= {'Accept': 'application/xml',
-                  'Content-Type': 'application/xml',
-                  'Authorization': 'Bearer ' + token},
-                verify=args.do_not_verify_ssl)
+        r = requests.get(
+            url + "/JSSResource/%s/id/%s" % (resource, resource_id),
+            headers={
+                "Accept": "application/xml",
+                "Content-Type": "application/xml",
+                "Authorization": "Bearer " + token,
+            },
+            verify=args.do_not_verify_ssl,
+        )
         tree = ET.fromstring(r.content)
 
-        if mode == 'ea':
-            if tree.find('input_type/type').text != 'script':
-                print('No script found in: %s' % tree.find('name').text)
+        if mode == "ea":
+            if tree.find("input_type/type").text != "script":
+                print("No script found in: %s" % tree.find("name").text)
                 get_script = False
                 # continue
 
         # Determine resource path (folder name)
-        resource_path = os.path.join(export_path, download_path ,tree.find('name').text)
+        resource_path = os.path.join(export_path, download_path, tree.find("name").text)
 
         # Check to see if it exists
         if os.path.exists(resource_path):
-            print("Resource is already in the repo: ", tree.find('name').text)
+            print("Resource is already in the repo: ", tree.find("name").text)
 
             if not overwrite:
-                print("\tSkipping: ", tree.find('name').text)
+                print("\tSkipping: ", tree.find("name").text)
                 continue
 
-
-        else:    # Make the folder
+        else:  # Make the folder
             os.makedirs(resource_path)
 
-        print('Saving: ', tree.find('name').text)
+        print("Saving: ", tree.find("name").text)
 
         # Create script string, and determine the file extension
         if get_script:
-            xmlstr = ET.tostring(tree.find(script_xml), encoding='unicode', method='text').replace('\r','')
-            if xmlstr.startswith('#!/bin/sh'):
-                ext = '.sh'
-            elif xmlstr.startswith('#!/usr/bin/env sh'):
-                ext = '.sh'
-            elif xmlstr.startswith('#!/bin/bash'):
-                ext = '.sh'
-            elif xmlstr.startswith('#!/usr/bin/env bash'):
-                ext = '.sh'
-            elif xmlstr.startswith('#!/bin/zsh'):
-                ext = '.sh'
-            elif xmlstr.startswith('#!/usr/bin/python'):
-                ext = '.py'
-            elif xmlstr.startswith('#!/usr/bin/env python'):
-                ext = '.py'
-            elif xmlstr.startswith('#!/usr/bin/perl'):
-                ext = '.pl'
-            elif xmlstr.startswith('#!/usr/bin/ruby'):
-                ext = '.rb'
+            xmlstr = ET.tostring(
+                tree.find(script_xml), encoding="unicode", method="text"
+            ).replace("\r", "")
+            if xmlstr.startswith("#!/bin/sh"):
+                ext = ".sh"
+            elif xmlstr.startswith("#!/usr/bin/env sh"):
+                ext = ".sh"
+            elif xmlstr.startswith("#!/bin/bash"):
+                ext = ".sh"
+            elif xmlstr.startswith("#!/usr/bin/env bash"):
+                ext = ".sh"
+            elif xmlstr.startswith("#!/bin/zsh"):
+                ext = ".sh"
+            elif xmlstr.startswith("#!/usr/bin/python"):
+                ext = ".py"
+            elif xmlstr.startswith("#!/usr/bin/env python"):
+                ext = ".py"
+            elif xmlstr.startswith("#!/usr/bin/perl"):
+                ext = ".pl"
+            elif xmlstr.startswith("#!/usr/bin/ruby"):
+                ext = ".rb"
             else:
-                print('No interpreter directive found for: ', tree.find('name').text)
-                ext = '.sh' # Call it sh for now so the uploader detects it
+                print("No interpreter directive found for: ", tree.find("name").text)
+                ext = ".sh"  # Call it sh for now so the uploader detects it
 
-            with open(os.path.join(resource_path, '%s%s' % (mode,ext)), 'w') as f:
+            with open(os.path.join(resource_path, "%s%s" % (mode, ext)), "w") as f:
                 f.write(xmlstr)
 
             # Need to remove ID and script contents and write out xml
             try:
                 tree.find(script_xml).clear()
-                tree.remove(tree.find('id'))
-                tree.remove(tree.find('script_contents_encoded'))
-                tree.remove(tree.find('filename'))
+                tree.remove(tree.find("id"))
+                tree.remove(tree.find("script_contents_encoded"))
+                tree.remove(tree.find("filename"))
             except:
                 pass
 
-        xmlstr = minidom.parseString(ET.tostring(tree, encoding='unicode', method='xml')).toprettyxml(indent="   ")
-        with open(os.path.join(resource_path, '%s.xml' % mode), 'w') as f:
+        xmlstr = minidom.parseString(
+            ET.tostring(tree, encoding="unicode", method="xml")
+        ).toprettyxml(indent="   ")
+        with open(os.path.join(resource_path, "%s.xml" % mode), "w") as f:
             f.write(xmlstr)
     invalidate_uapi_token(token)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Download Scripts from Jamf')
-    parser.add_argument('--url')
-    parser.add_argument('--username')
-    parser.add_argument('--password')
-    parser.add_argument('--export_path')
-    parser.add_argument('--overwrite', action='store_true') # Overwrites existing files
-    parser.add_argument('--do_not_verify_ssl', action='store_false') # Skips SSL verification
+
+if __name__ == "__main__":
+    # Export to current directory by default
+    export_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+
+    parser = argparse.ArgumentParser(description="Download Scripts from Jamf")
+    parser.add_argument("--url")
+    parser.add_argument("--username")
+    parser.add_argument("--password")
+    parser.add_argument("--export_path")
+    parser.add_argument("--overwrite", action="store_true")  # Overwrites existing files
+    parser.add_argument(
+        "--do_not_verify_ssl", action="store_false"
+    )  # Skips SSL verification
     args = parser.parse_args()
     # Get configs from files
-    CONFIG_FILE_LOCATIONS = ['jamfapi.cfg',os.path.expanduser('~/jamfapi.cfg')]
-    CONFIG_FILE = ''
+    CONFIG_FILE_LOCATIONS = ["jamfapi.cfg", os.path.expanduser("~/jamfapi.cfg")]
+    CONFIG_FILE = ""
     # Parse Config File
     CONFPARSER = configparser.ConfigParser()
     for config_path in CONFIG_FILE_LOCATIONS:
@@ -176,50 +201,45 @@ if __name__ == '__main__':
             print("Found Config: {0}".format(config_path))
             CONFIG_FILE = config_path
 
-    if CONFIG_FILE == "":
-        config_ = configparser.ConfigParser()
-        config_['jss'] = {}
-        config_['jss']['username'] = "username"
-        config_['jss']['password'] = "password"
-        config_['jss']['server'] = "server"
-        config_['jss']['export_path'] = "export_path"
-
-        with open('jamfapi.cfg', 'w') as configfile:
-            config_.write(configfile)
-        print("Config File Created. Please edit jamfapi.cfg and run again.")
-        
-        print("No Config File found!")
-        exit(0)
-    else:
-        # Read local directory, user home, then /etc/ for besapi.conf
-        CONFPARSER.read(CONFIG_FILE)
-        # If file exists
-        # Get config
-        username = CONFPARSER.get('jss', 'username')
-        password = CONFPARSER.get('jss', 'password')
-        url = CONFPARSER.get('jss', 'server')
+    if CONFIG_FILE != "":
         try:
-            export_path = CONFPARSER.get('jss', 'export_path')
+            # Get config
+            CONFPARSER.read(CONFIG_FILE)
         except:
-            # Export to current directory by default
-            export_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+            print("Can't read config file")
+        try:
+            username = CONFPARSER.get("jss", "username")
+        except:
+            print("Can't find username in configfile")
+        try:
+            password = CONFPARSER.get("jss", "password")
+        except:
+            print("Can't find password in configfile")
+        try:
+            url = CONFPARSER.get("jss", "server")
+        except:
+            print("Can't find url in configfile")
+        try:
+            export_path = CONFPARSER.get("jss", "export_path")
+        except:
+            print("Can't find export_path in config")
 
     # Ask for password if not supplied via command line args
     if args.password:
         password = args.password
     elif password is None:
         password = getpass.getpass()
-    
+
     if args.export_path:
         export_path = args.export_path
-    
+
     if args.url:
         url = args.url
 
     if args.username:
         username = args.username
-    
+
     # Run script download for extension attributes
-    download_scripts(overwrite=args.overwrite, mode='ea')
+    download_scripts(overwrite=args.overwrite, mode="ea")
     # Run script download for scripts
-    download_scripts(overwrite=args.overwrite, mode='script')
+    download_scripts(overwrite=args.overwrite, mode="script")
